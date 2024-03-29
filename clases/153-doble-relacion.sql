01337e1c29f8a65bc1deddec9254abbe2e80cdcd
Gustavo Vasquez <gustavo.vasquez@corhuila.edu.co>
2024-03-29 14:11:19 -0500

    Fin de la seccion 14

--- clases/153-doble-relacion.sql
+++
@@ -1,4 +0,0 @@
-
-select followers.*, leader.name as leader, follower.name as follower from followers
-INNER join "user" leader on leader.id = followers.leader_id
-INNER join "user" follower on follower.id = followers.follower_id;
